773389f0160fe6f9c8fda2a09768f69b77cb8423
Tim Nwungang <timnwungang@wtmba-002.local>
2023-12-11 19:02:53 +0800

    increasing version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="filecleaner",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple command line utility that organises files in a directory into subdirectories.",
     long_description=long_description,
     long_description_content_type="text/markdown",
